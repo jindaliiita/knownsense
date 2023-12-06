77eda05efdedbdff71a14914a3eb1cd56e588a90
piyushjindal <piyushjindal@adobe.com>
2023-12-06 23:05:03 +0530

    Franklin Sidekick

--- tools/sidekick/knownsense/keypressListener.js
+++ tools/sidekick/knownsense/keypressListener.js
@@ -1,10 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('keydown', function(event) {
-        // Check if the pressed key is an alphabet
-        event.stopPropagation();
-        if (/^[a-zA-Z0-9]$/.test(event.key)) {
-          console.log('pressed key:', event.key);
-          // Add your custom logic here
-        }
-      });
-  });
+if (navigator.clipboard) {
+  console.log('Clipboard API available');
+}
+
+function writeToClipboard(text, callback) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      // Success
+      if (callback && typeof callback === 'function') {
+        callback(null);
+      }
+    })
+    .catch((error) => {
+      // Error
+      if (callback && typeof callback === 'function') {
+        callback(error);
+      }
+    });
+}
+
+function readUsingNavigator(text, callback) {
+  navigator.clipboard.readText()
+    .then((resp) => {
+      // Success
+      if (callback && typeof callback === 'function') {
+        callback(resp);
+      }
+    })
+    .catch((error) => {
+      // Error
+      if (callback && typeof callback === 'function') {
+        callback(error);
+      }
+    });
+}
+
+writeToClipboard('This text is now in the clipboard', function (error) {
+  if (error) {
+    console.error('Error copying to clipboard:', error);
+  } else {
+    console.log('Text successfully copied to clipboard!');
+  }
+});
